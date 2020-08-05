09430681413c33981867a30dca3fc8d9f47a0cbb
jakcz <jakcz@DESKTOP-7QLTUE3.hitronhub.home>
2020-08-04 20:06:35 -0600

    added shit to Map and Tile classes

--- Summer/src/application/Map.java
+++ Summer/src/application/Map.java
@@ -1,5 +1,32 @@
 package application;
 
-public class Map {
+import java.util.ArrayList;
 
+public class Map {
+	int[][] grid = new int[10][10];
+	ArrayList<SpeedUp> speedUps = new ArrayList<SpeedUp>(5);
+	ArrayList<SpeedDown> speedDowns= new ArrayList<SpeedDown>(5);
+	Player player;
+	//Enemy boss;
+	void update() {
+		
+	};
+	
+	void updateTrap() {
+		
+	};
+	
+	void updatePowerUp() {
+		
+	};
+	
+	void checkTile() {
+		
+	};
+	
+	void triggerTile() {
+		
+	};
+	
+	
 }
--- Summer/src/application/Tile.java
+++ Summer/src/application/Tile.java
@@ -1,5 +1,15 @@
 package application;
 
 public class Tile {
-
+	String ID;
+	boolean canOverlap = false;
+	int x;
+	int y;
+	
+	
+	
+	void clear() {
+		
+	};
+	
 }
