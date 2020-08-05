3919e78f9f05384a275a1563c32987a990a01182
jakcz <jakcz@DESKTOP-7QLTUE3.hitronhub.home>
2020-08-04 22:03:03 -0600

    Some changes

--- Summer/src/application/Map.java
+++ Summer/src/application/Map.java
@@ -3,27 +3,45 @@ package application;
 import java.util.ArrayList;
 
 public class Map {
-	int[][] grid = new int[10][10];
-	ArrayList<SpeedUp> speedUps = new ArrayList<SpeedUp>(5);
-	ArrayList<SpeedDown> speedDowns= new ArrayList<SpeedDown>(5);
-	Player player;
-	//Enemy boss;
+	ArrayList<ArrayList<Tile>> grid = new ArrayList<ArrayList<Tile>>(); //grid of Tiles
+	ArrayList<SpeedUp> speedUps = new ArrayList<SpeedUp>(); //List of Tiles with speedUps
+	ArrayList<SpeedDown> speedDowns= new ArrayList<SpeedDown>(); //List of Tiles with speedDowns
+	Player player; //the player, duh
+	//Enemy boss; //the level's boss (only one level so far)
+	
+	/**
+	 * updates the map everytime the player or enemy moves
+	 * will call other update functions to check and update data accordingly
+	 * 
+	 */
 	void update() {
 		
 	};
 	
+	/**
+	 * updates the players speed (down)
+	 */
 	void updateTrap() {
 		
 	};
 	
+	/**
+	 * updates the players speed (up)
+	 */
 	void updatePowerUp() {
 		
 	};
 	
+	/**
+	 * checks whether player can move on the tile (cannot move on same tile as enemy)
+	 */
 	void checkTile() {
 		
 	};
 	
+	/**
+	 * activates trap/powerup on tile then calls clear() on tile
+	 */
 	void triggerTile() {
 		
 	};
--- Summer/src/application/Tile.java
+++ Summer/src/application/Tile.java
@@ -1,13 +1,16 @@
 package application;
 
 public class Tile {
-	String ID;
+	String ID; //blank, speedUp or speedDown
 	boolean canOverlap = false;
-	int x;
-	int y;
+	boolean hasTrap = false;
+	boolean hasPowerUp = false;
+	int x; //x position on grid
+	int y; //y position on grid
 	
-	
-	
+	/**
+	 * clears the trap/powerup from the tile
+	 */
 	void clear() {
 		
 	};
