f9e445fa9e7a07ce2df7da07fd4fa457bf8c65fd
FabulousFritz <jasminenguyen8169@gmail.com>
2020-08-05 16:18:54 -0600

    Added stuff to it

--- Summer/src/application/Player.java
+++ Summer/src/application/Player.java
@@ -1,8 +1,133 @@
 package application;
 
+import javafx.scene.input.KeyCode;
+
 public class Player{
+	//------------------------------------Initializing the necessary variables------------------------------------------
 	private String name;
 	private int score;
 	private int lives;
+	private int speed;
+	private Map map;
+	private KeyCode left;
+	private KeyCode right;
+	private KeyCode up;
+	private KeyCode down;
+	
+	//-------------------------------------------Getters and Setters----------------------------------------------------
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public int getLives() {
+		return lives;
+	}
+	
+	public void setLives(int lives) {
+		if(lives >= 0 && lives <= 3) {			
+			this.lives = lives;
+		}
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(int speed) {
+		if(speed >= 0) {			
+			this.speed = speed;
+		}
+	}
+	
+	public Map getMap() {
+		return map;
+	}
+	
+	public void setMap(Map map) {
+		this.map = map;
+	}
+	
+	public KeyCode getLeft() {
+		return left;
+	}
+
+	public void setLeft(KeyCode left) {
+		this.left = left;
+	}
+
+	public KeyCode getRight() {
+		return right;
+	}
+
+	public void setRight(KeyCode right) {
+		this.right = right;
+	}
+
+	public KeyCode getUp() {
+		return up;
+	}
+
+	public void setUp(KeyCode up) {
+		this.up = up;
+	}
+
+	public KeyCode getDown() {
+		return down;
+	}
+
+	public void setDown(KeyCode down) {
+		this.down = down;
+	}
+
+	//-----------------------------------------------Constructors-------------------------------------------------------
+	/**
+	 * Create a Player with a specific name
+	 * @param name
+	 */
+	public Player(String name) {
+		setName(name);
+		setScore(0);
+		setLives(3);
+		//TODO: Set speed
+		//TODO: Map
+		setLeft(KeyCode.A);
+		setRight(KeyCode.D);
+		setUp(KeyCode.W);
+		setDown(KeyCode.S);
+	}
+	
+	//----------------------------------------------------Methods-------------------------------------------------------
+	/**
+	 * Returns a boolean to represent whether a player is dead
+	 * @return true is the player is dead and false otherwise
+	 */
+	public boolean isDead() {
+		if(lives == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Decreases the player's speed by a specified value
+	 * @param value the amount the player's speed will decrease by
+	 */
+	public void slow(int value) {
+		setSpeed(speed - value);
+	}
+	
+	
 	
 }
