c8f9395ea209f5a64a1cb0be7f3d2de71d3f9a4a
jeffr <jeffr@Jeffery-PC.hitronhub.home>
2020-08-05 16:07:31 -0600

    Keyboard thingy

--- Summer/src/application/Main.java
+++ Summer/src/application/Main.java
@@ -1,17 +1,30 @@
 package application;
 	
+import java.util.Timer;
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
+	private AnimationTimer at = new AnimationTimer() {
+		
+		@Override
+		public void handle(long arg0) {
+			update();
+			
+		}
+	};
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
+			SpecialKeyboard.init(scene);
+			at.start();
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -19,6 +32,19 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	
+	public void update() {
+		System.out.println(SpecialKeyboard.activeLists.toString());
+		if(SpecialKeyboard.checkKey(KeyCode.A)) {
+			System.out.println("AAAAAA");
+		}
+		if(SpecialKeyboard.checkKey(KeyCode.B)) {
+			System.out.println("BBBBB");
+		}
+		
+	}
+	
+
 	
 	public static void main(String[] args) {
 		launch(args);
