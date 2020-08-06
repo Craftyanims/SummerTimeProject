8c871e9f97c70a2b67dc4124bfc78818f2231737
FabulousFritz <jasminenguyen8169@gmail.com>
2020-08-06 15:00:31 -0600

    enemy

--- Summer/src/application/Enemy.java
+++ Summer/src/application/Enemy.java
@@ -1,5 +1,11 @@
 package application;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
 public class Enemy{
+	private int speed;
+	private Map map;
+	private ArrayList<Point2D> pathPattern;
 
 }
