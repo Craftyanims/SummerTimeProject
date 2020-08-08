a0d92382f162c70a23858e0a365c2e91ee07493f
jeffr <jeffr@Jeffery-PC.hitronhub.home>
2020-08-07 23:43:18 -0600

    added basic info onto interaction stuff

--- Summer/src/application/Interaction.java
+++ Summer/src/application/Interaction.java
@@ -10,11 +10,10 @@ public abstract class Interaction extends Rectangle{
 	private Tile tile;
 	Interaction(Tile tile, int value){
 		super(0,0,Game.GRID_X*0.7,Game.GRID_X*0.7);
-		setFill(Color.rgb(0,0,255));
 		this.tile = tile;
 		this.value = value;
 	}
-	public abstract void update();
+	public abstract void trigger();
 	
 
 }
--- Summer/src/application/SpeedUp.java
+++ Summer/src/application/SpeedUp.java
@@ -1,13 +1,17 @@
 package application;
 
+import javafx.scene.paint.Color;
+
 public class SpeedUp extends Interaction{
-	
+	private int speedBuff = 15;
+	private int duration = 3000;
 	SpeedUp(Tile tile){
 		super(tile,10);
+		setFill(Color.rgb(255,0,0));
 	}
 
 	@Override
-	public void update() {
+	public void trigger() {
 		// TODO Auto-generated method stub
 		
 	}
